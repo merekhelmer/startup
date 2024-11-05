fc35d2bd1660b27cadc2baea924711e919801690
Merek Helmer <merek.helmer@byu.edu>
2024-11-04 20:21:21 -0700

    Integrated BrowserRouter in App.jsx to handle navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 // src/App.jsx
 import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './components/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Home } from './home/Home';
+import { Login } from './login/Login';
+import { Recommendations } from './recommendations/Recommendations';
+import { Results } from './results/Results';
 import './app.css';
 
-export default function App() {
+function App() {
   return (
-    <div className="body">
-      <Header />
-      <Home />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="body">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/recommendations" element={<Recommendations />} />
+          <Route path="/results" element={<Results />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
+
+export default App;
