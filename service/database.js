446c386d7ec0d97a34b68c14c4f5203a8ebc3460
Merek Helmer <merek.helmer@byu.edu>
2024-11-20 00:13:53 -0700

    Established MongoDB connection using Mongo Client - Created utility functions for user management

--- service/database.js
+++ service/database.js
@@ -1,11 +1,42 @@
 const { MongoClient } = require('mongodb');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
-const config = require('./dbConfig.json');
+const dbConfig = require('./dbConfig.json');
 
-const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const url = `mongodb+srv://${dbConfig.userName}:${dbConfig.password}@${dbConfig.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('startup');
-const userCollection = db.collection('users');
-const sessionCollection = db.collection('sessions');
-const voteCollection = db.collection('votes');
+const dbName = 'startupDB';
+const usersCollectionName = 'users';
+const sessionsCollectionName = 'sessions';
+
+let db;
+
+// initialize MongoDB connection
+(async function connectDB() {
+  try {
+    await client.connect();
+    db = client.db(dbName);
+    console.log(`Connected to MongoDB database: ${dbName}`);
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+  }
+})();
+
+// User functions
+async function getUser(email) {
+  return db.collection(usersCollectionName).findOne({ email });
+}
+
+async function getUserByToken(token) {
+  return db.collection(usersCollectionName).findOne({ token });
+}
+
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+  const token = uuid.v4();
+
+  const user = { email, password: passwordHash, token };
+  await db.collection(usersCollectionName).insertOne(user);
+  return user;
+};
