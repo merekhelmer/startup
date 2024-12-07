d5197295f580f0464416ee6a74ba23ca9fe5bc39
Merek Helmer <merek.helmer@byu.edu>
2024-12-06 21:34:03 -0700

    Websocket package and vite config changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,14 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:4000',
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+      },
+      '/ws': {
+        target: 'http://localhost:4000',
+        ws: true,
+      },
     },
-  },
+  }
 });
